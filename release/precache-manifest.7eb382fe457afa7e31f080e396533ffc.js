b23cc56afcaaf2a636f1e73545f06d9ff3b99445
taotao <taotao@mianchao-inc.com>
2020-05-29 15:59:09 +0800

    xx

---
+++ release/precache-manifest.7eb382fe457afa7e31f080e396533ffc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec266fd3acc00963fe8b40427047d2b",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/release_index.ejs"
+  },
+  {
+    "revision": "e6ea9dff8ee039d7b978",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/css/2.819092da.chunk.css"
+  },
+  {
+    "revision": "98fdd0454a754c181c3c",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/css/main.f51e900d.chunk.css"
+  },
+  {
+    "revision": "e6ea9dff8ee039d7b978",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/js/2.ab7ed732.chunk.js"
+  },
+  {
+    "revision": "98fdd0454a754c181c3c",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/js/main.f5f11538.chunk.js"
+  },
+  {
+    "revision": "5a4c4bd16d4678ba7929",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/js/runtime-main.f45591cf.js"
+  },
+  {
+    "revision": "4ad07e23aa6dcea1cb54dbfc396681e1",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/media/bg.4ad07e23.png"
+  },
+  {
+    "revision": "c268bab5a96dceae66414ca06d6bf7ec",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/media/jt.c268bab5.png"
+  },
+  {
+    "revision": "0dea0f9739d68fcff939ba51d73a1e53",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/media/lbx.0dea0f97.png"
+  },
+  {
+    "revision": "bab2b509158ad04982f4527a1f3445fe",
+    "url": "//st-q.mxcdn.cn/mianchao_hzdata/static/media/map.bab2b509.png"
+  }
+]);
